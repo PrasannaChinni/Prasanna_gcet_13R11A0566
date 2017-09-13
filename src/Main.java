cc8ecaa0fb4f46e83b2b41f8ca1c2985f0f8e070
PrasannaChinni <31918667+PrasannaChinni@users.noreply.github.com>
2017-09-13 16:59:38 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		System.out.print("Epam ");
+		System.out.println("test");
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
